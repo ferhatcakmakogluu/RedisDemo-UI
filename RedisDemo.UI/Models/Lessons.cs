777dd0e8e337e9b92cd6e76c464c3d7e62ed5457
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-25 17:40:36 +0300

    initial commit, models created

---
+++ RedisDemo.UI/Models/Lessons.cs
@@ -0,0 +1,10 @@
+﻿namespace RedisDemo.UI.Models
+{
+    public class Lessons
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal CourseCredit { get; set; }
+    }
+}
---
+++ RedisDemo.UI/Models/Students.cs
@@ -0,0 +1,11 @@
+﻿namespace RedisDemo.UI.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string SchoolNo { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
